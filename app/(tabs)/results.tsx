7811833f83542d7ae536fbf87d91a00c9c808414
hieuha3010 <hieuha3010@gmail.com>
2025-09-26 12:26:03 +0700

    Updated results.tsx

--- app/(tabs)/results.tsx
+++ app/(tabs)/results.tsx
@@ -341,7 +341,7 @@ const createStyles = () => StyleSheet.create({
     justifyContent: 'space-between',
   },
   resultsCount: {
-    fontSize: theme.typography.subhead.fontSize,
+    fontSize: 11,
     lineHeight: theme.typography.subhead.lineHeight,
     fontWeight: '500',
     color: theme.colors.textMuted,
