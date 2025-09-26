8a8f1bb5f0d1e384a8efa6f1b130ffc1f05dc4b9
hieuha3010 <hieuha3010@gmail.com>
2025-09-26 12:45:21 +0700

    Updated index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -63,7 +63,7 @@ export default function SearchScreen() {
         <View style={styles.headerWrap}>
           <Header
             title="MemoryHub"        
-            subtitle="We remember everything"   
+            subtitle="Relax, we remember it all!"   
           />
         </View>
 
